bfab5146a6063f0a5cb9b840008af0de1b95d85f
ayushqui <proayush736@gmail.com>
2025-10-09 17:55:57 +0000

    Revert "Role re-evaluation should respect ask every time choice by user"
    This reverts commit dc685a63ca7686e7bfc10fc0351c66bb8976cbcc.

--- PermissionController/src/com/android/permissioncontroller/role/model/Permissions.java
+++ PermissionController/src/com/android/permissioncontroller/role/model/Permissions.java
@@ -254,7 +254,6 @@ public class Permissions {
             // If we've granted a permission which wasn't granted, it's no longer user set or fixed.
             newMask |= PackageManager.FLAG_PERMISSION_USER_FIXED
                     | PackageManager.FLAG_PERMISSION_USER_SET;
-                    | PackageManager.FLAG_PERMISSION_ONE_TIME;
         }
         // If a component gets a permission for being the default handler A and also default handler
         // B, we grant the weaker grant form. This only applies to default permission grant.
@@ -609,7 +608,6 @@ public class Permissions {
         if (!overrideUserSetAndFixed) {
             fixedFlags |= PackageManager.FLAG_PERMISSION_USER_FIXED
                     | PackageManager.FLAG_PERMISSION_USER_SET;
-                    | PackageManager.FLAG_PERMISSION_ONE_TIME;
         }
         return (flags & fixedFlags) != 0;
     }
